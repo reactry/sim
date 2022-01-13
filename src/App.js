3cb2d4db6e3a69238b304c062b2b5643123b8b87
Ankur Seth <iankurseth@gmail.com>
2022-01-14 00:18:20 +0530

    Installed tailwind

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
+        <p className="bg-slate-600 text-white text-2xl p-20">
           CMX
         </p>
       </header>
